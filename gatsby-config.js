b2ccb01ef29860dfe4c89d4df292d774058ab158
Julian Anderson <julianjca11@gmail.com>
2020-05-24 10:48:11 +0700

    adding fathom analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,18 @@ module.exports = {
   siteMetadata: {
     title: `@juliancanderson's public notes`,
   },
-  plugins: ['gatsby-theme-andy', `gatsby-plugin-postcss`],
+  plugins: ['gatsby-theme-andy', `gatsby-plugin-postcss`, {
+    resolve: 'gatsby-plugin-fathom',
+    options: {
+      // Fathom server URL. Defaults to `cdn.usefathom.com`
+      trackingUrl: 'analytics.juliancanderson.com',
+      // Unique site id
+      siteId: 'CFHOL',
+      // Domain whitelist
+      whitelistHostnames: [
+        'notes.juliancanderson.com',
+        'juliancanderson.com'
+      ]
+    }
+  }],
 };
